c0e8f35d2bc8ee0da9cca7b358fa80bc7595d3f8
Marija Ferber <marija.ferber@getbraintree.com>
2021-10-26 18:25:41 +0000

    WIP

--- app/lib/location_requester.rb
+++ app/lib/location_requester.rb
@@ -1,59 +1,30 @@
 require 'uri'
 require 'net/http'
-#
-# uri = URI('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-# res = Net::HTTP.get_response(uri)
-# puts res.body if res.is_a?(Net::HTTPSuccess)
-#
-#
-# uri = URI('https://api.nasa.gov/planetary/apod')
-# params = { :api_key => 'your_api_key' }
-# uri.query = URI.encode_www_form(params)
-#
-# res = Net::HTTP.get_response(uri)
-# puts res.body if res.is_a?(Net::HTTPSuccess)
-
 
 class LocationRequester
   # self. makes it a class method
-  def self.get_api_response(name)
-    uri = URI("http://localhost:8000/locations")
+  def self.get_api_response(name, limit=10)
+    raise ArgumentError, 'too many HTTP redirects' if limit == 0
+    # uri_str = 'http://localhost:8000/locations/?name=earth'
+    uri_str = 'http://localhost:8000/locations'
+
+    uri = URI(uri_str)
     params = {:name => name}
     uri.query = URI.encode_www_form(params)
-    r = Net::HTTP.get_response(uri)
-    require "pry"; binding.pry
-    # puts res.body if res.is_a?(Net::HTTPSuccess)
-    #
-    #
-    # TODO: we need redirection for this response!!
-    #
-    #
-  # def fetch(uri_str, limit = 10)
-  #   # You should choose a better exception.
-  #   raise ArgumentError, 'too many HTTP redirects' if limit == 0
-  #
-  #   response = Net::HTTP.get_response(URI(uri_str))
-  #
-  #   case response
-  #   when Net::HTTPSuccess then
-  #     response
-  #   when Net::HTTPRedirection then
-  #     location = response['location']
-  #     warn "redirected to #{location}"
-  #     fetch(location, limit - 1)
-  #   else
-  #     response.value
-  #   end
-  # end
-  #
-  # print fetch('http://www.ruby-lang.org')
+    response = Net::HTTP.get_response(uri)
 
+    case response
+    when Net::HTTPSuccess then response.body.to_json
+    when Net::HTTPRedirection then
+      location = response['location']
+      warn "redirected to #{location}"
+      uri = URI(location)
+      uri.query = URI.encode_www_form(params)
+      response = Net::HTTP.get_response(uri).body.to_json
+
+      # fetch(location, limit - 1)
+    else
+      response.body.to_json
+    end
   end
-
-    # @classmethod
-    # def get_api_response(cls, name):
-    #     URL = "http://localhost:8000/locations"
-    #     r = requests.get(url=URL, params={"name": name})
-    #     data = r.json()
-    #     return data
 end
--- spec/integration/location_requester_spec.rb
+++ spec/integration/location_requester_spec.rb
@@ -6,8 +6,8 @@ describe "LocationRequester" do
     response = LocationRequester.get_api_response(name: name)
     expect(response["name"].class).to eq(String)
     expect(response["coordinates"].class).to eq(Hash)
-    expect(response["coordinates"]["x"]).to eq(Int)
-    expect(response["coordinates"]["y"]).to eq(Int)
-    expect(response["coordinates"]["z"]).to eq(Int)
+    expect(response["coordinates"]["x"]).to eq(Integer)
+    expect(response["coordinates"]["y"]).to eq(Integer)
+    expect(response["coordinates"]["z"]).to eq(Integer)
   end
 end
